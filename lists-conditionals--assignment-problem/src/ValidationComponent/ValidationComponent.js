b64e771cdf3ef880cc810dd353ee2b1e05fbb6dc
roylee0704 <roylee0704@gmail.com>
2020-09-06 11:36:23 +0700

    fix min text length

--- lists-conditionals--assignment-problem/src/ValidationComponent/ValidationComponent.js
+++ lists-conditionals--assignment-problem/src/ValidationComponent/ValidationComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default (props) => {
-    const result = props.textLength > props.minTextLength ? 'Text long enough' : 'Text too short';
+    const result = props.textLength >= props.minTextLength ? 'Text long enough' : 'Text too short';
     return (
         <p>{result}</p>
     );
